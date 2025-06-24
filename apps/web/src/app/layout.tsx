ec545de60cb32afcfd4f6bc5e443e66267f90f74
Mary Mettille <mary.mettille@startribune.com>
2025-06-24 00:00:43 -0500

    Fix hydration issue

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <NavBar />
       <body
         className={`antialiased`}
       >
+        <NavBar />
         {children}
       </body>
     </html>
