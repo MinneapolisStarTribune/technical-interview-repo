0a46d577606645193e13c5e33a19d838c196f424
Mary Mettille <mary.mettille@startribune.com>
2025-06-23 23:43:35 -0500

    Breaking News Article

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import ArticleList from "../northern-star/ArticleList/ArticleList";
 import { articles } from "../hardcoded-data/articles";
+import BreakingNews from "../northern-star/BreakingNews/BreakingNews";
 
 export default function HomePage() {
   // Group articles by first category type
@@ -15,12 +16,25 @@ export default function HomePage() {
     {}
   );
 
-  // Sorted categories
+  // Find first breaking news article
+  const breakingArticle = articles.find((a) => a.isBreaking);
+
+  // I need to remove the breaking article from the list, otherwise I see it twice.
+  if (breakingArticle) {
+    for (const category in articlesByCategory) {
+      articlesByCategory[category] = articlesByCategory[category].filter(
+        (a) => a.slug !== breakingArticle.slug
+      );
+    }
+  }
+
   const categories = Object.keys(articlesByCategory).sort();
 
   return (
     <main className="min-h-screen bg-gray-50 py-10 px-4">
       <div className="max-w-7xl mx-auto">
+        <BreakingNews article={breakingArticle || {}} />
+
         {categories.map((category) => (
           <ArticleList
             key={category}
