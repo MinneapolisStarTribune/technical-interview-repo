9849227b84036396ca04ecd120fed5a5437c48c2
Mary Mettille <mary.mettille@startribune.com>
2025-06-23 22:10:08 -0500

    Fight with Jest one more time
    In tsconfig.json, we need the line that reads `"jsx": "preserve"` for a development build but to run a test with Jest and Typescript, that line needs to read `"jsx": "react-jsx",` or Jest won't be able to transform the JSX.
    
    This commit adds babel to our repo to let us keep `preserve` for development builds and to run tests side-by-side.

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -2,18 +2,15 @@ const { createDefaultPreset } = require("ts-jest");
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest', // Use ts-jest preset for TypeScript + JSX support
-  testEnvironment: 'jest-environment-jsdom', // Simulate browser environment
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest', 
+  },
   moduleNameMapper: {
-    // Handle CSS imports (optional, adjust if you have other asset types)
     '\\.(css|scss|sass)$': 'identity-obj-proxy',
   },
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest', // Transform TypeScript and TSX files with ts-jest
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'], // Ignore these paths
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Optional: for extra setup like jest-dom
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import NavBar from "../northern-star/NavBar/NavBar"
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Northern Star",
@@ -30,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <NavBar />
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
       </body>
