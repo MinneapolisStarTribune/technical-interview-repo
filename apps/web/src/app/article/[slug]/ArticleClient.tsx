677bd304da6abae0a8f1031ad457b61145d7ffe1
Mary Mettille <mary.mettille@startribune.com>
2025-06-26 12:10:01 -0500

    Link article author name to author page

--- apps/web/src/app/article/[slug]/ArticleClient.tsx
+++ apps/web/src/app/article/[slug]/ArticleClient.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link"
+import { authors } from "../../../hardcoded-data/authors";
 interface Article {
   slug: string;
   title: string;
@@ -14,11 +16,20 @@ interface ArticleClientProps {
 }
 
 export default function ArticleClient({ article }: ArticleClientProps) {
+  const author = authors.find((author) => author.name === article.author);
+
   return (
     <main className="max-w-4xl mx-auto p-6">
       <h1 className="text-4xl font-bold mb-4 text-primary-emerald-green">{article.title}</h1>
       <p className="text-sm text-gray-500 mb-6">
-        By {article.author} · {article.date}
+        By{" "}       
+          <Link
+            href={author ? `/author/${author.slug}` : "#"}
+            className="hover:underline"
+          >
+            {article.author}
+          </Link>
+        {" "}· {article.date}
       </p>
       {article.image && (
         <img
